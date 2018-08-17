c062d55459d12102491dd512f98ddb74d2335392
BerryThDev <40075398+BerryThDev@users.noreply.github.com>
2018-08-16 20:54:26 -0400

    init

---
+++ login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Login</title>
+</head>
+<h3>Please login</h3>
+<p>Enter your username and password below.</p>
+<form method = "post" action="login.php">
+  Username:<br/>
+  <input type="username" name="username"><br/>
+  Password:<br/>
+  <input type="password" name="password"><br/>
+  <input type="submit" value="Login">
+</form>
+</html>
